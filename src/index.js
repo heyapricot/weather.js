9d29135598c32ededb53e63f12a44cf2b9d2aa71
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-08 17:40:02 -0600

    Can Retrieve weather data specifying city and unit type

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './style.css';
 
 import DataFetcher from './scenes/Main/components/DataFetcher/DataFetcher';
 
-DataFetcher.getWeatherData();
+DataFetcher.getWeatherData('Mexico City', 'imperial').then(data => console.log(data));
+DataFetcher.getWeatherData('Mexico City', 'metric').then(data => console.log(data));
--- src/scenes/Main/components/DataFetcher/DataFetcher.js
+++ src/scenes/Main/components/DataFetcher/DataFetcher.js
@@ -1,12 +1,12 @@
-const DataFetcher = ((cityString) => {
-  const getWeatherData = async function () {
+const DataFetcher = (() => {
+  const getWeatherData = async function (cityString, units) {
     try {
-      const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=76df2138d28e29b9db95ca3a883b65ad', { mode: 'cors' });
+      const url = 'http://api.openweathermap.org/data/2.5/weather';
+      const APIKey = '76df2138d28e29b9db95ca3a883b65ad';
+      const request = `${url}?q=${cityString}&APPID=${APIKey}&units=${units}`;
+      const response = await fetch(request, { mode: 'cors' });
       if (response.ok) {
-        const weatherData = response.json()
-          .then((response) => {
-            console.log(response);
-          });
+        return await response.json();
       }
     } catch (error) {
       console.log(error);
