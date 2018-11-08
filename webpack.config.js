6a4afdce37a05255e591a2ab427310217c2f3e35
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-08 11:28:12 -0600

    Additional setup for Webpack to build the index.html file and use Bootstrap as an npm package

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather.js',
+    }),
+  ],
+  devtool: 'source-map',
+};
