eb5d9dfd9c4db134557aaff643da4051ec63fe3d
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-12 11:39:13 -0600

    Main scene can display font awesome icon

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import DataFetcher from './scenes/Main/components/DataFetcher/DataFetcher';
 // DataFetcher.getWeatherData('Mexico City', 'imperial').then(data => console.log(data));
 // DataFetcher.getWeatherData('Mexico City', 'metric').then(data => console.log(data));
 
-const mainSection = document.createElement('section');
-mainSection.id = 'main';
-const container = HTMLElem('div', ['container', 'h-100'], mainSection);
+const mainSection = HTMLElem('section', [], document.body);
+mainSection.node.id = 'main';
+const container = HTMLElem('div', ['container', 'h-100'], mainSection.node);
 const row = HTMLElem('div', ['row', 'h-100'], container.node);
 const column = HTMLElem('div', ['col', 'd-flex', 'flex-column', 'justify-content-center'], row.node);
+const icon = HTMLElem('i', ['fas', 'fa-temperature-high', 'fa-10x'], column.node);
+icon.node.style.color = 'white';
 const form = CityForm();
 column.node.appendChild(form.node);
-document.body.appendChild(mainSection);
--- src/scenes/Main/components/HTMLElem/HTMLElem.js
+++ src/scenes/Main/components/HTMLElem/HTMLElem.js
@@ -1,4 +1,4 @@
-const HTMLElem = (nodeType, cssClasses, parentNode = NaN) => {
+const HTMLElem = (nodeType, cssClasses = [], parentNode = NaN) => {
   const node = document.createElement(nodeType);
   cssClasses.forEach(cssClass => node.classList.toggle(cssClass));
   if (!Number.isNaN(parentNode)) {
