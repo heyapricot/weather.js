de31422f42ed5fd4b3ae72be4a9be53a697f040a
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-12 12:09:06 -0600

    Added submit button to form

--- src/scenes/Main/components/CityForm/CityForm.js
+++ src/scenes/Main/components/CityForm/CityForm.js
@@ -3,8 +3,11 @@ import HTMLElem from '../HTMLElem/HTMLElem';
 const CityForm = () => {
   const form = HTMLElem('form', []);
   const { node } = form;
-  const cityInput = HTMLElem('input', ['form-control'], node);
+  const cityInput = HTMLElem('input', ['form-control', 'my-3'], node);
   cityInput.node.placeholder = 'Please enter your city';
+  const submit = HTMLElem('button', ['btn', 'btn-primary'], node);
+  submit.node.textContent = 'Submit';
+  submit.node.type = 'submit';
   return { node };
 };
 
