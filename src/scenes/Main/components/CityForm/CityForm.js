4eb8a32e4f5943a2a84fd917f80861d25c434a25
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-12 12:36:21 -0600

    Created function to add callbacks that will be called on click and a function to get the values from the form's inputs

--- src/scenes/Main/components/CityForm/CityForm.js
+++ src/scenes/Main/components/CityForm/CityForm.js
@@ -1,14 +1,23 @@
 import HTMLElem from '../HTMLElem/HTMLElem';
 
 const CityForm = () => {
+  const clickCallbacks = [];
   const form = HTMLElem('form', []);
   const { node } = form;
   const cityInput = HTMLElem('input', ['form-control', 'my-3'], node);
   cityInput.node.placeholder = 'Please enter your city';
-  const submit = HTMLElem('button', ['btn', 'btn-primary'], node);
-  submit.node.textContent = 'Submit';
-  submit.node.type = 'submit';
-  return { node };
+  const button = HTMLElem('div', ['btn', 'btn-primary'], node);
+  button.node.textContent = 'Submit';
+
+  const addClickCallback = callbackfn => clickCallbacks.push(callbackfn);
+  const getInputData = () => [cityInput.node].map(input => input.value);
+  const onClickSubmit = () => {
+    clickCallbacks.forEach(callbackfn => callbackfn());
+  };
+
+  button.node.addEventListener('click', onClickSubmit);
+
+  return { addClickCallback, getInputData, node };
 };
 
 export default CityForm;
