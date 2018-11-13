f4219dc29db42b0ab5db3b35300fcfdd0703a7a9
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-13 16:56:22 -0600

    Changed appearance of buttons. Gave UnitPicker a toggler look and feel

--- src/scenes/Main/components/CityForm/CityForm.js
+++ src/scenes/Main/components/CityForm/CityForm.js
@@ -9,9 +9,9 @@ const CityForm = () => {
   const cityInput = HTMLElem('input', ['form-control'], inputGroup.node);
   cityInput.node.placeholder = 'Please enter your city';
   const buttonContainer = HTMLElem('div', ['input-group-append'], inputGroup.node);
-  const button = HTMLElem('div', ['btn', 'btn-primary'], node);
+  const button = HTMLElem('div', ['btn', 'btn-light'], node);
   button.node.textContent = 'Submit';
-  const unitPicker = UnitPicker(['ºC', 'ºF'], Array(2).fill(['btn', 'btn-info']));
+  const unitPicker = UnitPicker(['ºC', 'ºF'], [['btn', 'btn-primary'], ['btn', 'btn-info']], ['metric', 'imperial']);
   buttonContainer.node.appendChild(unitPicker.node);
 
   const addClickCallback = callbackfn => clickCallbacks.push(callbackfn);
@@ -19,10 +19,11 @@ const CityForm = () => {
   const onClickSubmit = () => {
     clickCallbacks.forEach(callbackfn => callbackfn());
   };
+  const unitSystem = () => unitPicker.activeButton().value;
 
   button.node.addEventListener('click', onClickSubmit);
 
-  return { addClickCallback, getInputData, node };
+  return { addClickCallback, getInputData, node, unitSystem };
 };
 
 export default CityForm;
--- src/scenes/Main/components/CityForm/components/UnitPicker/UnitPicker.js
+++ src/scenes/Main/components/CityForm/components/UnitPicker/UnitPicker.js
@@ -1,27 +1,37 @@
 import HTMLElem from '../../../HTMLElem/HTMLElem';
 
 const UnitPicker = (textContent, cssClasses, values) => {
+  let activeIndex = 0;
+  const clickCallbacks = [];
   const buttonGroup = HTMLElem('div', ['btn-group']);
+  let buttons = [];
+  const activeButton = () => buttons[activeIndex];
+  const addClickCallback = callbackfn => clickCallbacks.push(callbackfn);
+  const onClick = () => clickCallbacks.forEach(callbackfn => callbackfn());
   const { node } = buttonGroup;
   const buttonParameters = textContent.map(
     (text, index) => ({ text, style: cssClasses[index], value: values[index] }),
   );
 
+  const toggleAppearance = () => {
+    buttons.forEach(button => button.node.classList.toggle('active'));
+    activeIndex = (activeIndex + 1) % buttons.length;
+  };
 
-  const buttons = buttonParameters.map((parameters, index) => {
-    const clickCallbacks = [];
+  addClickCallback(toggleAppearance);
+
+  buttons = buttonParameters.map((parameters, index) => {
     const button = HTMLElem('div', parameters.style, buttonGroup.node);
+    if (index === 0) button.node.classList.toggle('active');
     button.node.textContent = parameters.text;
-    const addClickCallback = callbackfn => clickCallbacks.push(callbackfn);
-    const onClick = () => clickCallbacks.forEach(callbackfn => callbackfn());
-    const closure = () => console.log(`I'm button ${index}`);
-    const value = () => parameters;
-    addClickCallback(closure);
     button.node.addEventListener('click', onClick);
-    return { addClickCallback, ...button, value };
+    return { ...button, ...parameters };
   });
 
-  return { buttons, node };
+  return {
+    activeButton, addClickCallback, buttons, node,
+  };
 };
+
 
 export default UnitPicker;
