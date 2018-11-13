41498913f0159899964f8bea79007bfc8bbefaea
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-13 11:03:14 -0600

    Main scene uses the new WeatherDisplay module to show the current temperature in the city that is written on the input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import CityForm from './scenes/Main/components/CityForm/CityForm';
 import DataFetcher from './scenes/Main/components/DataFetcher/DataFetcher';
 import HTMLElem from './scenes/Main/components/HTMLElem/HTMLElem';
+import WeatherDisplay from './scenes/Main/components/WeatherDisplay/WeatherDisplay';
 import './style.scss';
 
 // DataFetcher.getWeatherData('Mexico City', 'imperial').then(data => console.log(data));
@@ -11,12 +12,11 @@ mainSection.node.id = 'main';
 const container = HTMLElem('div', ['container', 'h-100'], mainSection.node);
 const row = HTMLElem('div', ['row', 'h-100'], container.node);
 const column = HTMLElem('div', ['col', 'd-flex', 'flex-column', 'justify-content-center', 'text-center'], row.node);
-const icon = HTMLElem('i', ['fas', 'fa-temperature-high', 'fa-10x'], column.node);
-icon.node.style.color = 'white';
+column.node.appendChild(WeatherDisplay.node);
 const form = CityForm();
 form.node.classList.toggle('my-5');
 const onClickSubmit = () => {
-  DataFetcher.getWeatherData(form.getInputData().city, 'metric').then(data => console.log(data));
+  DataFetcher.getWeatherData(form.getInputData().city, 'metric').then(data => WeatherDisplay.setTemperature(data.main.temp));
 };
 form.addClickCallback(onClickSubmit);
 column.node.appendChild(form.node);
