8900446752aa62fa61c34b1d6feb6b3a655fcbc0
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-13 15:55:01 -0600

    Added method to UnitPicker buttons to run callbacks on click

--- src/scenes/Main/components/CityForm/components/UnitPicker/UnitPicker.js
+++ src/scenes/Main/components/CityForm/components/UnitPicker/UnitPicker.js
@@ -1,15 +1,24 @@
 import HTMLElem from '../../../HTMLElem/HTMLElem';
 
-const UnitPicker = (textContent, cssClasses) => {
+const UnitPicker = (textContent, cssClasses, values) => {
   const buttonGroup = HTMLElem('div', ['btn-group']);
   const { node } = buttonGroup;
   const buttonParameters = textContent.map(
-    (text, index) => ({ text, style: cssClasses[index] }),
+    (text, index) => ({ text, style: cssClasses[index], value: values[index] }),
   );
-  const buttons = buttonParameters.map((parameters) => {
-    const button = HTMLElem('div', parameters.style, node);
+
+
+  const buttons = buttonParameters.map((parameters, index) => {
+    const clickCallbacks = [];
+    const button = HTMLElem('div', parameters.style, buttonGroup.node);
     button.node.textContent = parameters.text;
-    return button;
+    const addClickCallback = callbackfn => clickCallbacks.push(callbackfn);
+    const onClick = () => clickCallbacks.forEach(callbackfn => callbackfn());
+    const closure = () => console.log(`I'm button ${index}`);
+    const value = () => parameters;
+    addClickCallback(closure);
+    button.node.addEventListener('click', onClick);
+    return { addClickCallback, ...button, value };
   });
 
   return { buttons, node };
