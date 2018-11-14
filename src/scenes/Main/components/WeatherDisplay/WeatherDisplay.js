7c850bc4a24066ec65ff3ad8305be52ce071a7ae
Alvaro Sanchez <heyalvaro@gmail.com>
2018-11-14 10:44:39 -0600

    Added topText to Weather Display that says 'Current temperature:'

--- src/scenes/Main/components/WeatherDisplay/WeatherDisplay.js
+++ src/scenes/Main/components/WeatherDisplay/WeatherDisplay.js
@@ -4,12 +4,16 @@ import './styles.scss';
 const WeatherDisplay = (() => {
   const container = HTMLElem('div', ['container']);
   const { node } = container;
+  node.id = 'temperatureText';
   const icon = HTMLElem('i', ['fas', 'fa-thermometer-three-quarters', 'fa-10x'], node);
   icon.node.style.color = 'white';
+  const topText = HTMLElem('h2', [], node);
   const textDisplay = HTMLElem('h1', ['display-3'], node);
-  textDisplay.node.id = 'temperatureText';
   const units = () => ({ metric: 'ºC', imperial: 'ºF' });
-  const setTemperature = (temperature, system = units().metric) => { textDisplay.node.textContent = `${temperature} ${system}`; };
+  const setTemperature = (temperature, system = units().metric) => {
+    topText.node.textContent = 'Current temperature:';
+    textDisplay.node.textContent = `${temperature} ${system}`;
+  };
 
   return { node, setTemperature, units };
 })();
