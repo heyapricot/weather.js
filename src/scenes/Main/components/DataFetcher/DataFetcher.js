a83b503622b66633d6dd585eedd508f0ab1dc8e8
Alvaro Sanchez <heyapricot@fastmail.com>
2021-03-24 11:09:04 -0600

    The app uses https to request weather data from openweather API. Updated dependencies

--- src/scenes/Main/components/DataFetcher/DataFetcher.js
+++ src/scenes/Main/components/DataFetcher/DataFetcher.js
@@ -1,9 +1,9 @@
 const DataFetcher = (() => {
   const getWeatherData = async function (cityString, units) {
     try {
-      const url = 'http://api.openweathermap.org/data/2.5/weather';
+      const url = 'https://api.openweathermap.org/data/2.5/weather';
       const APIKey = '76df2138d28e29b9db95ca3a883b65ad';
-      const request = `${url}?q=${cityString}&APPID=${APIKey}&units=${units}`;
+      const request = `${url}?q=${cityString}&appid=${APIKey}&units=${units}`;
       const response = await fetch(request, { mode: 'cors' });
       if (response.ok) {
         return await response.json();
